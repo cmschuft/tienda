097988b693f8ff6919934c9d5c54e008115cdcaa
Cesar Matias C. Schuft <cmschuft@gmail.com>
2022-06-23 12:38:22 -0300

    Agregando JS

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let libro
+
+libro = "Mi Librito"
+
+let otraVariable = "Mi Otro Valor"
+
+console.log ("Hola")
+
+console.log (libro)
+
+const dia = "Jueves"
+
+console.log (dia)
+
+let mensaje = "Este es mi mensaje"
+
+let mensaje2 = "Este es mi otro mensaje"
+
+console.log (mensaje + " " + mensaje2)
+
+mensaje2 = "Otra cosa"
+
+console.log (mensaje2)
+
+/* let nombre = prompt ("Decime tu Nombre")
+
+console.log (nombre)
+
+let apellido = prompt ("Decime tu Apellido")
+
+alert (apellido) */
+
+/* El prompt devuelve STRING - El parseInt lo transforma a Número */
+
+/* const num1 = parseInt (prompt ("Ingresá el Primer Número")) 
+const num2 = parseInt (prompt ("Ingresá el Segundo Número"))
+
+console.log (num1 + num2)
+
+let resultado = (num1 + num2)
+
+alert ("Hola! La suma es igual a: " + resultado + " Muchas gracias! =)") */
