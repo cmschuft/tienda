152e21bc7a56fb5aaf42097aec564eea1ba78ef3
Cesar Matias C. Schuft <cmschuft@gmail.com>
2022-07-17 19:39:50 -0300

    Arrays + Primera Entrega

--- js/arrays.js
+++ js/arrays.js
@@ -26,14 +26,27 @@ addToCart.pop();
 
 console.log("Eliminaste del Carrito: Golosina Stick");
 
-console.log("El índice del primer producto eliminado es: " + addToCart.indexOf("Alpiste"));
+console.log("El índice del producto eliminado es: " + addToCart.indexOf("Alpiste"));
 
 /* Array de Objetos */
 
-const producto1 = {id: 1, nombre: "Alpiste", precio: "$90.00"};
+const producto1 = {id: 1, nombre: "Alpiste", precio: 90.00};
 
-const cart = [producto1, {id: 2, producto: "Pasta de Crias", precio: "$300.00"}];
+const cart = [producto1, {id: 2, nombre: "Pasta de Crias", precio: 300.00}];
 
-cart.push({id: 3, producto: "Golosina", precio: "$40.00"});
+cart.push({id: 3, nombre: "Golosina", precio: 40.00});
 
+console.log("El Carrito contiene:");
 console.log(cart);
+
+/* Búsqueda y Filtrado en Array */
+
+const search = cart.find((el) => el.nombre === 'Alpiste');
+
+console.log("Elemento Encontrado:");
+console.log(search);
+
+const filter = cart.filter((el) => el.precio < 300);
+
+console.log("Los productos que cuestan menos de $300 son:");
+console.log(filter);
